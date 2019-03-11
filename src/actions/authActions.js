1f5ddfc787acda7eb697e9b8233345c826084a07
PierreDV <p.verteuil@gmail.com>
2019-03-11 19:03:57 +0100

    add missing semicolons

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -2,4 +2,4 @@ import { AUTH_USER_REQUEST } from './types';
 
 export const signup = ({ email, password }) => dispatch => {
 
-}
+};
--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -16,27 +16,27 @@ export const fetchPostLinks = () => dispatch => {
     })
     .catch(error => {
       dispatch(fetchPostLinksError(error.message));
-    })
-}
+    });
+};
 
 
 const fetchPostLinksRequest = () => {
-  return { type: FETCH_POST_LINKS_REQUEST }
-}
+  return { type: FETCH_POST_LINKS_REQUEST };
+};
 
 const fetchPostLinksSuccess = res => {
   return { 
     type: FETCH_POST_LINKS_SUCCESS,
     payload: res
-  }
-}
+  };
+};
 
 const fetchPostLinksError = error => {
   return {
     type: FETCH_POST_LINKS_ERROR,
     error
-  }
-}
+  };
+};
 
 export const fetchPost = id => dispatch => {
   dispatch(fetchPostRequest());
@@ -47,24 +47,24 @@ export const fetchPost = id => dispatch => {
     })
     .catch(error => {
       dispatch(fetchPostError(error.message));
-    })
-}
+    });
+};
 
 
 const fetchPostRequest = () => {
-  return { type: FETCH_POST_REQUEST }
-}
+  return { type: FETCH_POST_REQUEST };
+};
 
 const fetchPostSuccess = res => {
   return {
     type: FETCH_POST_SUCCESS,
     payload: res
-  }
-}
+  };
+};
 
 const fetchPostError = error => {
   return {
     type: FETCH_POST_ERROR,
     error
-  }
-}
+  };
+};
