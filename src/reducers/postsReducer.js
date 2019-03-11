e9ec1755c3e42ead99c3dea85ba12529c5ff2abc
PierreDV <p.verteuil@gmail.com>
2019-03-11 16:12:21 +0100

    export constants from action creators and make use of them in posts reducer

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,3 +1,12 @@
+import {
+  FETCH_POST_LINKS_REQUEST,
+  FETCH_POST_LINKS_SUCCESS,
+  FETCH_POST_LINKS_ERROR,
+  FETCH_POST_REQUEST,
+  FETCH_POST_SUCCESS,
+  FETCH_POST_ERROR
+} from '../actions/index';
+
 const INITIAL_STATE = {
   links: [],
   activePost: null,
@@ -7,41 +16,41 @@ const INITIAL_STATE = {
 
 export default (state = INITIAL_STATE, action) => {
   switch(action.type) {
-  case 'FETCH_POST_LINKS_REQUEST':
+  case FETCH_POST_LINKS_REQUEST:
     return {
       ...state,
       isFetching: true,
       errorMessage: null
     };
-  case 'FETCH_POST_LINKS_SUCCESS':
+  case FETCH_POST_LINKS_SUCCESS:
     return {
       ...state,
       isFetching: false,
       links: action.payload,
       errorMessage: null
     }
-  case 'FETCH_POST_LINKS_ERROR':
+  case FETCH_POST_LINKS_ERROR:
     return {
       ...state,
       isFetching: false,
       links: [],
       errorMessage: action.error
     }
-  case 'FETCH_POST_REQUEST':
+  case FETCH_POST_REQUEST:
     return {
       ...state,
       activePost: null,
       isFetching: true,
       errorMessage: null
     }
-  case 'FETCH_POST_SUCCESS':
+  case FETCH_POST_SUCCESS:
     return {
       ...state,
       isFetching: false,
       activePost: action.payload,
       errorMessage: null
     }
-  case 'FETCH_POST_ERROR':
+  case FETCH_POST_ERROR:
     return {
       ...state,
       isFetching: false,
