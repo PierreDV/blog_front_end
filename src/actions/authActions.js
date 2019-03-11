c0503087c49f2eac11d77067d30c9005553ec2b7
PierreDV <p.verteuil@gmail.com>
2019-03-11 22:02:31 +0100

    add auth token to Redux store on successfull user singup

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -1,11 +1,16 @@
-import { AUTH_USER_REQUEST } from './types';
+import { 
+  AUTH_USER_REQUEST,
+  AUTH_USER_SUCCESS
+} from './types';
 
-export const signup = (formProps) => dispatch => {
-  fetch('http://localhost:8080/api/v1/users/', {
+export const signup = (formProps) => async dispatch => {
+  const response = await fetch('http://localhost:8080/api/v1/users/', {
     method: "POST",
     body: JSON.stringify(formProps),
     headers:{
       'Content-Type': 'application/json'
     }
-  })
+  });
+
+  dispatch({ type: AUTH_USER_SUCCESS, payload: response.data.token })
 };
