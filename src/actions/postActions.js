cb25e98f71b2a7ffe83e896014ff789f0c2dd4c7
PierreDV <p.verteuil@gmail.com>
2019-03-27 18:43:33 +0100

    add post create form and action

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -5,7 +5,10 @@ import {
   FETCH_POST_LINKS_ERROR,
   FETCH_POST_REQUEST,
   FETCH_POST_SUCCESS,
-  FETCH_POST_ERROR
+  FETCH_POST_ERROR,
+  CREATE_POST_REQUEST,
+  CREATE_POST_SUCCESS,
+  CREATE_POST_FAILURE
 } from './types';
 
 export const fetchPostLinks = () => async dispatch => {
@@ -38,6 +41,28 @@ export const fetchPost = id => async dispatch => {
     dispatch({ 
       type: FETCH_POST_ERROR, 
       error: error.message 
+    });
+  }
+};
+
+export const createPost = (formProps, callback) => async (dispatch, getState) => {
+  try {
+    const { auth } = getState();
+    dispatch({ type: CREATE_POST_REQUEST });
+    const response = await axios.post(
+      'http://localhost:8080/api/v1/blog_posts/',
+      formProps,
+      {
+        headers: {
+          'x-access-token': auth.authenticated
+        }
+      }
+    );
+    callback();
+  } catch(error) {
+    dispatch({ 
+      type: CREATE_POST_ERROR, 
+      payload: error.message 
     });
   }
 };
--- src/containers/CreatePostForm.js
+++ src/containers/CreatePostForm.js
@@ -1,12 +1,53 @@
 import React, { Component } from 'react';
-import requireAuth from './requireAuth';
+import { reduxForm, Field } from 'redux-form';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { createPost } from 'actions/postActions';
 
-class CreatePostForm extends Component {
+class CreatePost extends Component {
+  onSubmit = formProps => {
+    this.props.createPost(formProps, () => {
+      this.props.history.push('/');
+    });
+  };
+
   render() {
-    return(
-      <div>Post Create Form</div>
+    const { handleSubmit } = this.props;
+
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit)}>
+        <fieldset>
+          <label>Title</label>
+          <Field
+            name="title"
+            type="text"
+            component="input"
+            autoComplete="none"
+          />
+        </fieldset>
+        <fieldset>
+          <label>Body</label>
+          <Field 
+            name="body_text"
+            type="text"
+            component="input"
+            autoComplete="none"
+          />
+        </fieldset>
+        <div>
+          {this.props.errorMessage}
+        </div>
+        <button>Create Post</button>
+      </form>
     );
   }
+} 
+
+function mapStateToProps(state) {
+  return { errorMessage: state.auth.errorMessage };
 }
 
-export default requireAuth(CreatePostForm);
+export default compose(
+  connect(mapStateToProps, { createPost }),
+  reduxForm({ form: 'createPost' })
+)(CreatePost);
