e010a89903ae3b3bc9d7fe47f5f2e0b4e78078f1
PierreDV <p.verteuil@gmail.com>
2019-03-30 18:46:08 +0100

    revert change to backend url

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -12,7 +12,7 @@ export const signup = (formProps, callback) => async dispatch => {
   try {
     dispatch({ type: AUTH_USER_REQUEST });
     const response = await axios.post(
-      `${process.env.BACK_END_URL}/api/v1/users/`, 
+      `${backEndUrl}/api/v1/users/`, 
       formProps
     );
     if(!response.statusText === "OK") throw response;
@@ -35,7 +35,7 @@ export const signin = (formProps, callback) => async dispatch => {
   try {
     dispatch({ type: AUTH_USER_REQUEST });
     const response = await axios.post(
-      `${process.env.BACK_END_URL}/api/v1/users/login`, 
+      `${backEndUrl}/api/v1/users/login`, 
       formProps
     );
     if(!response.statusText === "OK") throw response;
