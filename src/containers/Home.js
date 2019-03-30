4d02c3be69d07642d8b5dc4fbdb6e2baba108a69
PierreDV <p.verteuil@gmail.com>
2019-03-30 21:50:28 +0100

    remove uneeded import

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
 import { fetchPostLinks } from 'actions/postActions';
 import PostList from 'components/PostList';
 
