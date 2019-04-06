1a63a6e0bcd4046b2424634699bb78c544a0ed4b
PierreDV <p.verteuil@gmail.com>
2019-04-06 13:24:53 +0200

    hard code back end url until i can find a solution to using environment variables on github pages

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -10,11 +10,13 @@ import {
   CONFIRM_EMAIL_ERROR
 } from './types';
 
+const backEndUrl = (process.env.NODE_ENV === 'development') ? 'http://localhost:8080' : 'https://safe-meadow-41895.herokuapp.com';
+
 export const signup = (formProps, callback) => async dispatch => {
   try {
     dispatch({ type: AUTH_USER_REQUEST });
     const response = await axios.post(
-      `${process.env.BACK_END_URL}/api/v1/users/`, 
+      `${backEndUrl}/api/v1/users/`, 
       formProps
     );
     if(!response.statusText === "OK") throw response.message;
@@ -37,7 +39,7 @@ export const signin = (formProps, callback) => async dispatch => {
   dispatch({ type: AUTH_USER_REQUEST });
   try {
     const response = await axios.post(
-      `${process.env.BACK_END_URL}/api/v1/users/login`, 
+      `${backEndUrl}/api/v1/users/login`, 
       formProps
     );
     if(!response.statusText === "OK") throw response;
@@ -66,7 +68,7 @@ export const signout = () => {
 export const confirmEmail = (token , callback) => async dispatch => {
   try {
     const response = await axios.post(
-      `${process.env.BACK_END_URL}/api/v1/verify_user`,
+      `${backEndUrl}/api/v1/verify_user`,
       {token}
     );
     if(!response.statusText === "OK") throw response;
